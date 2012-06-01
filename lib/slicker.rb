c198a0038d66b22868fe91b56cfbdcb1276a1b6b
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-06-01 16:38:53 +0100

    Setting up the config hash so it's more like Sanitize.

--- lib/slicker.rb
+++ lib/slicker.rb
@@ -2,38 +2,37 @@ require 'sanitize'
 
 class Slicker
 
-  def self.protect(params)
+  def self.protect(params, config = {})
     if params.is_a?(String)
-      Sanitize.clean(params)
+      Sanitize.clean(params, config)
     elsif params.is_a?(Array)
-      walk_array(params)
+      walk_array(params, config)
     elsif params.is_a?(Hash)
-      walk_hash(params)
+      walk_hash(params, config)
     end
   end
 
-
-  def self.walk_hash(hash)
+  def self.walk_hash(hash, config)
     hash.keys.each do |key|
       if hash[key].is_a? String
-        hash[key] = Sanitize.clean(hash[key])
+        hash[key] = Sanitize.clean(hash[key], config)
       elsif hash[key].is_a? Hash
-        hash[key] = walk_hash(hash[key])
+        hash[key] = walk_hash(hash[key], config)
       elsif hash[key].is_a? Array
-        hash[key] = walk_array(hash[key])
+        hash[key] = walk_array(hash[key], config)
       end
     end
     hash
   end
 
-  def self.walk_array(array)
+  def self.walk_array(array, config)
     array.each_with_index do |el,i|
       if el.is_a? String
-        array[i] = Sanitize.clean(el)
+        array[i] = Sanitize.clean(el, config)
       elsif el.is_a? Hash
-        array[i] = walk_hash(el)
+        array[i] = walk_hash(el, config)
       elsif el.is_a? Array
-        array[i] = walk_array(el)
+        array[i] = walk_array(el, config)
       end
     end
     array
